fd34bcf9212eb50169910093681911cd6e6349e2
Abdullah Mert ÇANKAYA <mertcanmert1515@gmail.com>
2024-11-07 22:40:43 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  darkMode: 'selector',
+  content: [
+    './**/*.{html,js,ts,jsx,tsx}',  // HTML, JS, TS, JSX ve TSX dosyalarını kontrol et
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
